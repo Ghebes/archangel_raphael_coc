67956dce62c82e5d7138c45f6ececbf38a1ffc94
Michael Ghebranious <ghebraniousmike@gmail.com>
2024-01-22 19:47:42 -0600

    Initial Commit

---
+++ Archangel Raphael/Archangel_RaphaelApp.swift
@@ -0,0 +1,17 @@
+//
+//  Archangel_RaphaelApp.swift
+//  Archangel Raphael
+//
+//  Created by Michael Ghebranious on 1/22/24.
+//
+
+import SwiftUI
+
+@main
+struct Archangel_RaphaelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
