b16504cc51e2e125d25a67086f969e0b53d89c27
Michael Ghebranious <ghebraniousmike@gmail.com>
2024-01-23 19:02:47 -0600

    Home Screen

---
+++ Archangel Raphael/HomePage.swift
@@ -0,0 +1,18 @@
+//
+//  HomePage.swift
+//  Archangel Raphael
+//
+//  Created by Michael Ghebranious on 1/23/24.
+//
+
+import SwiftUI
+
+struct HomePage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomePage()
+}
