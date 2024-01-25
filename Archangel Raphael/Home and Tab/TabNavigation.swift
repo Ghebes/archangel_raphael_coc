a593e98d05a95de22bd81446b203a62b60a7d82a
Michael Ghebranious <ghebraniousmike@gmail.com>
2024-01-25 13:44:55 -0600

    Working on Tab

---
+++ Archangel Raphael/Home and Tab/TabNavigation.swift
@@ -0,0 +1,18 @@
+//
+//  TabNavigation.swift
+//  Archangel Raphael
+//
+//  Created by Michael Ghebranious on 1/25/24.
+//
+
+import SwiftUI
+
+struct TabNavigation: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TabNavigation()
+}
